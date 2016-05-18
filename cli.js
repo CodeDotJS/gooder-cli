0d62129d1d214bc9bb198ef2908f997e22af9292
CodeDotJS <rushforlinux@gmail.com>
2016-05-18 13:51:59 +0530

    dns part

--- cli.js
+++ cli.js
@@ -3,32 +3,38 @@
 'use strict';
 
 const gooder = require('gooder');
-
 const https = require('follow-redirects').https;
-
 const mkdirp = require('mkdirp');
-
-const updateNotifier = require('update-notifier');
-
-const pkg = require('./package.json');
-
-updateNotifier({pkg}).notify();
-
 const colors = require('colors/safe');
-
+const fs = require('fs');
+const http = require('follow-redirects').http;
 const argv = require('yargs')
 	.usage(colors.cyan.bold('\nUsage: $0 <command/option> [target]'))
-
 	.command(' u', ' ❱ required before URL')
-
 	.demand(['u',])
-
 	.describe('u', ' ❱ Google Drive URL')
-
 	.example('$0 -u https://drive.google.com...xyz')
-
 	.argv;
 
-const startURL = argv.u.toString();
+const updateNotifier = require('update-notifier');
+const pkg = require('./package.json');
+updateNotifier({pkg}).notify();
 
-const trimPart = gooder(startURL);
+function checkInternet(cb) {
+	require('dns').lookup('google.com', err => {
+		if (err && err.code === 'ENOTFOUND') {
+			cb(false);
+		} else {
+			cb(true);
+		}
+	});
+}
+
+checkInternet(isConnected => {
+	if (isConnected) {
+		console.log(colors.cyan.bold('\n ❱ Internet Connection   :    ✔\n'));
+	} else {
+		console.log(colors.red.bold('\n ❱ Internet Connection   :    ✖\n'));
+		process.exit(1);
+	}
+});
