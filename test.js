ad01a4ab4da22a79b37fa792c8f3009d8b55b253
CodeDotJS <rushforlinux@gmail.com>
2016-05-19 04:30:14 +0530

    1 passed

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 import childProcess from 'child_process';
+
 import test from 'ava';
 
 test.cb(t => {
-	childProcess.execFile('./cli.js', ['whatiz'], {
+	childProcess.execFile('./cli.js', ['-u https://drive.google.com/file/d/0B9PN3IJWm3bOSUhCaFJyTzZ4eTg/view'], {
 		cwd: __dirname
 	}, (err, stdout) => {
 		t.ifError(err);
